5f4d25953a61a4dd29dd926035f727f47b8cb01a
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-04 22:18:10 -0700

    Manually remove precompilation cache for PyCall

--- src/julia_venv/install_deps.jl
+++ src/julia_venv/install_deps.jl
@@ -31,6 +31,22 @@ let venv_depot = joinpath(@__DIR__, "depot")
         elseif command == "build"
             Pkg.build("PyCall")
         elseif command == "compile"
+            # Manually removing PyCall's precmopile file since it may
+            # not be detected as "stale" by julia when it was compiled
+            # in non-PyJulia -mode.
+            pkg = Base.identify_package("PyCall")
+            if pkg !== nothing
+                cache_files = Base.find_all_in_cache_path(pkg)
+                if !isempty(cache_files) &&
+                        startswith(cache_files[1], venv_depot)
+                    @info """
+                    Removing old compilation cache for PyCall:
+                    $(cache_files[1])
+                    """
+                    rm(cache_files[1], force = true)
+                end
+            end
+            @info "Compiling PyCall..."
             @eval Main import PyCall
         else
             error("Unknown command $command")
