bf742c5215264ec798e2a644aa26577255492b23
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-05 20:48:25 -0700

    Monkey patch only in the "root" julia-venv

--- src/julia_venv/startup.jl
+++ src/julia_venv/startup.jl
@@ -1,4 +1,11 @@
-if !any(isequal(@__DIR__), Base.LOAD_PATH)
-    pushfirst!(Base.LOAD_PATH, @__DIR__)
+# Do monkey patches (via JuliaVenv) only in the "root" julia-venv
+# process. `Base.compilecache` uses `Base.load_path_setup_code` to
+# propagate so there is no need to setup `Base.DEPOT_PATH` manually
+# inside child processes launched by `Base.compilecache`.
+if get(ENV, "_JULIA_VENV_PROCESS", "") != "true"
+    if !any(isequal(@__DIR__), Base.LOAD_PATH)
+        pushfirst!(Base.LOAD_PATH, @__DIR__)
+    end
+    @eval using JuliaVenv
 end
-using JuliaVenv
+ENV["_JULIA_VENV_PROCESS"] = "true"
